98efa2ef5f7c6bbcf5f170d3f7be693f980dd772
sobree mama <sobreemama@MacBook-Air-khxng-sobree.local>
2025-01-28 04:18:48 +0700

    add input search for response

--- src/app/pages/page.tsx
+++ src/app/pages/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React,{useState,useEffect} from "react";
-import { faChevronDown, faPlus, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown, faPlus, faSearch, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ModelBlog from "@/components/modelblog";
 import CardCompnent from "@/components/card";
@@ -33,17 +33,25 @@ export default function Page(){
     const [isBlogData, setIsBlogData] = useState<DetailData[]>(getBlogerAll)
     const [selectedCategory, setSelectedCategory] = useState<string>('ทั้งหมด')
     const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+    const [isSearchMs, setIsSearchMs] = useState<boolean>(false)
     const handleCategorySelect = (category: string) => {
       setSelectedCategory(category);
       setIsDropdownOpen(false);
 
       if (category === 'ทั้งหมด') {
-        window.location.href = 'http://localhost:3000/pages';
+        window.location.href = '/pages';
       } else {
-        window.location.href = `http://localhost:3000/pages?category=${category}`;
+        window.location.href = `/pages?category=${category}`;
       }
     };
     
+    const handleStatusResize = () => {
+      if (!isSearchMs) return;
+      const screenWidth = window.innerWidth;
+      if (screenWidth > 600) {
+        setIsSearchMs(false);
+      }
+    };
     useEffect(()=>{
       setSelectedCategory(path ?? "ทั้งหมด")
       if(path){
@@ -64,9 +72,16 @@ export default function Page(){
         const handleResize = ()=> setScreenWidth(window.innerWidth)
         setScreenWidth(window.innerWidth);
         window.addEventListener("resize", handleResize)
-        return ()=> window.removeEventListener("resize", handleResize)
 
-    },[])
+        if(isSearchMs){
+          window.addEventListener("resize",handleStatusResize)
+        }
+        
+        return ()=> {
+          window.removeEventListener("resize", handleResize)
+          window.removeEventListener("resize",handleStatusResize)
+        }
+    },[isSearchMs])
     return (
         <div className="p-3 w-full">
             <div className="flex flex-row w-full">
@@ -85,10 +100,34 @@ export default function Page(){
                     </div>
                 </div>
                 ):(
-                <div className="w-auto px-2 py-2">
-                    <FontAwesomeIcon icon={faSearch} className="w-20 h-5 text-gray-400"/>
+                <div className="w-auto py-2">
+                  {isSearchMs ? (
+                    <button className="bg-none font-bold" type="button" onClick={()=>setIsSearchMs(false)}>
+                      <FontAwesomeIcon className="font-bold w-20 h-6 text-gray-400" icon={faXmark}/>
+                    </button>
+                  ):(
+                    <button type="button" className="px-2 bg-none" onClick={()=>setIsSearchMs(true)}>
+                      <FontAwesomeIcon icon={faSearch} className="w-20 h-5 text-gray-400"/>
+                    </button>
+                  )}
                 </div>)}
+                {isSearchMs ? (
+                  <div className="w-full flex">
+                     <div className="relative w-full">
+                        <div className="absolute inset-y-0  start-0 flex items-center ps-3 pointer-events-none">
+                            <FontAwesomeIcon icon={faSearch} className="w-4 h-4 text-gray-400"/>
+                        </div>
+                        <input 
+                            type="text" 
+                            className="block w-full py-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                            placeholder="Search" 
+                            onChange={(e)=>setSearch(e.target.value)}
+                        />
+                    </div>
+                  </div>
+                ):(
                     <div className="flex" style={{width:"100%"}}>
+                      
                         <div className="relative inline-block sm:w-40">
                           <button
                             onClick={() => setIsDropdownOpen((prev) => !prev)}
@@ -118,6 +157,7 @@ export default function Page(){
                         </button>
 
                     </div>
+                  )}
             </div>
             <div className="flex flex-col w-full lg:w-2/3 mt-3">
                 {isBlogData && isBlogData.filter((result:DetailData)=>{
